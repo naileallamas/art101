630044018b85c28222c0b33356514bfb7973013b
Nailea (he/they) <79344903+naileallamas@users.noreply.github.com>
2022-04-18 17:29:22 -0700

    lab 4 and 5

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,18 @@
-// Summary. (use period)
-// Description. (use period)
+// This is pseudocode for the game Pong.
+// Two player game where person controls their paddle to hitthe pong ball.
 // @link   URL
-// @file   This files defines the MyClass class.
-// @author AuthorName.
-// @since  x.x.x
+// @file   lab4/lab.js.
+// @author NaileaLlamas.
+// @since  4.18.20022
+
+//Outlice board parameters
+  //Assign each player a paddle
+    //Accept each user’s input of the paddle (up and down)
+
+  //Insert pong, have it restricted within the board
+    //Pong bounces off player paddles.
+
+  //If Pong passes player’s paddle,
+    //A point is given to opposing player
+
+  //Player with 11 points first wins
---
+++ lab5/js/lab.js
@@ -0,0 +1,26 @@
+// Declaring variables practice.
+// Declare make, model, color, year, declare a boolean, then reference them.
+// @link   URL
+// @file  lab5/lab.js
+// @author NaileaLlamas.
+// @since  4.18.22
+
+//Define Variables
+var make, model, color, year, age;
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year = 1995;
+
+//Calculate
+age = 2022 - year;
+
+//Create Boolean
+//Boolean ownit = true;
+
+//Print
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
